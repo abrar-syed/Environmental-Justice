29a4b72ccd3feb913f85992d4ee94de4a092f6a9
Kelly Armijo <karmijo@users.noreply.github.com>
2020-04-23 17:57:49 -0700

    updating tables

--- ca_cap_trade.sql
+++ ca_cap_trade.sql
@@ -16,25 +16,23 @@ CREATE TABLE `RACE` (
 	`Total_pop` int(12) default NULL,
 	`White` int(12) default NULL,
 	`Black` int(12) default NULL,
-	`Native` int(12) default NULL,
+	`American_indian` int(12) default NULL,
 	`Asian` int (12) default NULL,
-	`Hawaiian_islander` int(12) default NULL,
-	`Other` int(12) default NULL,
+	`Hawaiian` int(12) default NULL,
 	`Hispanic` int(12) default NULL,
+	`Other` int(12) default NULL,
+	`Two_or_more` int(12) default NULL,
 	PRIMARY KEY (`Census_ID`)
 	KEY `CENSUS` (`Census_ID`));
 --
--- Table structure for table `INCOME`
+-- Table structure for table `POVERTY`
 --
 
-DROP TABLE IF EXISTS `INCOME`;
-CREATE TABLE `INCOME` (
+DROP TABLE IF EXISTS `POVERTY`;
+CREATE TABLE `POVERTY` (
 	`Census_ID` tinyint(11) NOT NULL,
-	`Q1` int(12) default NULL,
-	`Q2` int(12) default NULL,
-	`Q3` int(12) default NULL,
-	`Q4` int(12) default NULL,
-	`Q5` int (12) default NULL,
+	`Total_pop` int(12) default NULL,
+	`Poverty` int(12) default NULL,
 	PRIMARY KEY (`Census_ID`),
 	KEY `CENSUS` (`Census_ID`));
 
@@ -46,15 +44,15 @@ DROP TABLE IF EXISTS `FACILITY_GHG_EMISSIONS`;
 CREATE TABLE `FACILITY_GHG_EMISSIONS` (
 	`CARB_ID` tinyint(6) NOT NULL,
 	`Facility` varchar(255) NOT NULL,
-	`Year` char(4) NOT NULL
-	`Total_co2` int(10) default NULL,
+	`Year` char(4) NOT NULL,
+	`Total_co2` float(25) default NULL,
+	`Covered_Emissions` float(25) default NULL,
+	`Non-Covered Emissions` float(25) default NULL,
 	`City` varchar(255) default NULL,
 	`State` varchar(255) default NULL,
 	`Zip` char(5) default NULL,
-	`County_ID` tinyint(5) NOT NULL,
 	`Class_ID` tinyint(6) default NULL,
 	PRIMARY KEY (`CARB_ID`),
-	KEY `COUNTY` (`County_ID`),
 	KEY `GHG_OFFSET_CREDITS` (`CARB_ID`),
 	KEY `FACILITY_CLASS` (`Class_ID`));
 
@@ -65,19 +63,9 @@ CREATE TABLE `FACILITY_GHG_EMISSIONS` (
 DROP TABLE IF EXISTS `FACILITY_CLASS`;
 CREATE TABLE `FACILITY_CLASS` (
 	`Class_ID` tinyint(6) NOT NULL,
-	`Class` varchar(255) NOT NULL
+	`Class` varchar(255) NOT NULL,
+	`Industry` varchar(255) NOT NULL, 
 	PRIMARY KEY (`Class_ID`));
-
---
--- Table structure for table `FACILITY_INDUSTRY`
---
-
-DROP TABLE IF EXISTS `FACILITY_INDUSTRY`;
-CREATE TABLE `FACILITY_INDUSTRY` (
-	`Industry` varchar(255) NOT NULL,
-	`Class_ID` tinyint(6) NOT NULL,
-	PRIMARY KEY (`Industry`),
-	KEY `FACILITY_CLASS` (`Class_ID`));
 
 --
 -- Table structure for table `CES`
@@ -85,11 +73,12 @@ CREATE TABLE `FACILITY_INDUSTRY` (
 
 DROP TABLE IF EXISTS `CES`;
 CREATE TABLE `CES` (
+	`CES_ID` tinyint(11) NOT NULL AUTO_INCREMENT,
 	`Indicator` varchar(255) NOT NULL,
 	`Value` float(6) NOT NULL,
 	`Percentile` float(6) NOT NULL,
 	`Census_ID` tinyint(11) NOT NULL,
-	PRIMARY KEY (`Indicator`),
+	PRIMARY KEY (`CES_ID`),
 	KEY `CENSUS` (`Census_ID`));
 
 --
@@ -100,26 +89,74 @@ DROP TABLE IF EXISTS `CES_INDICATOR`;
 CREATE TABLE `CES_INDICATOR` (
 	`Indicator` varchar(255) NOT NULL,
 	`Description` varchar(255) NOT NULL,
+	`Sub_Component` varchar(255) NOT NULL,
 	`Component` varchar(255) NOT NULL,
 	PRIMARY KEY (`Indicator`),
-	KEY `CES` (`Indicator`),
-	KEY `CES_COMPONENT` (`Component`));
+	KEY `CES` (`Indicator`));
 
 --
--- Table structure for table `CES_COMPONENT`
+-- Load RACE data
 --
 
-DROP TABLE IF EXISTS `CES_COMPONENT`;
-CREATE TABLE `CES_COMPONENT` (
-	`Component` varchar(255) NOT NULL,
-	PRIMARY KEY (`Component`));
+LOAD DATA LOCAL INFILE 'race_table.csv'
+INTO TABLE RACE
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Total_pop, White, Black, American_indian, Asian, Hawaiian, Hispanic, Other, Two_or_more, Census_ID);
 
+--
+-- Load POVERTY data
+--
 
+LOAD DATA LOCAL INFILE 'poverty_table.csv'
+INTO TABLE POVERTY
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Total_pop, Poverty, Census_ID);
 
+--
+-- Load FACILITY_GHG_EMISSIONS data
+--
 
+LOAD DATA LOCAL INFILE 'facility_table.csv'
+INTO TABLE FACILITY_GHG_EMISSIONS
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
+--
+-- Load FACILITY_CLASS data
+--
 
+LOAD DATA LOCAL INFILE 'facility_class_table.csv'
+INTO TABLE FACILITY_CLASS
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Class, Industry, Class_ID);
 
+--
+-- Load CES data
+--
+
+LOAD DATA LOCAL INFILE 'ces_table_final.csv'
+INTO TABLE CES
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Census_ID, Indicator, Value, Percentile);
+
+--
+-- Load CES_INDICATOR data
+--
+
+LOAD DATA LOCAL INFILE 'CES_Components.csv'
+INTO TABLE CES_INDICATOR
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
 
 
